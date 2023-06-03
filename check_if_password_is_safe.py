ccd4ddb9b50adc7bb4c4d201115934d47fd34585
JimmyShyaka <jimmydshyaka@gmail.com>
2023-06-03 08:42:55 -0500

    more explanations in README

--- check_if_password_is_safe.py
+++ check_if_password_is_safe.py
@@ -7,29 +7,26 @@ We use a hashing function to hash the password we want to check and
 then we split the hashed value into 2 parts: The first part is the first 5 characters of the hash
 and the second part is the rest of the characters of the hash that we call "tail".
 
-We use the first five characters of the hash to check if the public database
-of hacked/leaked passwords to see if the first 5 characters of the hashed value
-of the password appeared in the public database there and return a list of
-all hashed strings that are found to match our first five hashes.
+We check if the first 5 characters of the hashed value of the password appeared in the public
+database there and return a list of all hashes of passwords that are found to match our first five hash characters.
 
-Why all of that process? The answer is that we don't want to query the database
-using all hashed characters of our password because that would be a vulnerability
-if we disclosed the hashed value of the password we want to check.
+Why all of that process? The answer is that we don't want to query the database using all hashed
+characters of our password because that would be a vulnerability if we sent to that public database
+a query with the entire hashed value of the password we want to check.
 
 It would be a vulnerability because we are sending that hashed value over the
 public internet, a bad actor could find out the hashed value of the password
-we are checking. That bad actor can reverse engineer the hashed value and
-find out our password.
+we are checking. That bad actor can reverse engineer the password using that hashed value.
 
 Instead, we will query the public database using the first 5 characters
 of the hashed value of our password. Then we will return a long list
 of all hashed values found in the database that match our password's first 5 hashed values.
-We will have the list of those hashes on our machine locally which is safe
-and then count how many times the tail part of our password hashed value
-appeared in that list we just got from the public database.
 
-We will then count how many times the tail part is found in the list and print it
-on the screen. That count is how many times our password appeared in the database.
+We will have the list of those hashes on our machine locally which is safe
+and then count how many times the "tail" part of our password hash (which we never shared with anyone)
+appeared in that list we just got from the public database. That count is how many
+times our password appeared in the public database of leaked passwords.
+
 If the password is not found in the database, then the program will exit with
 a message letting you know that "the password was not found. The password is safe. Carry on!"
 
