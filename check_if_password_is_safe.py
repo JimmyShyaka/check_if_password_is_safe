82f01b683114280bd5eba4de355046a8d8408021
JimmyShyaka <jimmydshyaka@gmail.com>
2023-06-03 08:17:06 -0500

    updated readme

--- check_if_password_is_safe.py
+++ check_if_password_is_safe.py
@@ -20,6 +20,7 @@
 
 import requests   #import these 2 libraries
 import hashlib
+
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
